6445d208eca17dd3cca9e5df816163f7562a0450
FlameRain926 <73727228+FlameRain926@users.noreply.github.com>
2022-12-25 15:33:55 +0800

    Update Complain.java

--- manager/src/main/com/example/manage/bean/Complain/Complain.java
+++ manager/src/main/com/example/manage/bean/Complain/Complain.java
@@ -15,6 +15,10 @@ public class Complain {
     private int id;
     private String owners_name; // 业主名字
     private String content; //投诉内容
+    private String dispatcher_name; //调度员名字
+    private String worker_name; //工人名字
+    private String dispatcher_explain; //调度员的解释
+    private String worker_explain; //工人的解释
 
 
 }
