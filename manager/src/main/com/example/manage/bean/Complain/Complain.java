60619a778d201fc0f906162d2ba60338a44c527d
ting <845669893@qq.com>
2022-12-29 11:12:03 +0800

    ComplainTest

--- manager/src/main/com/example/manage/bean/Complain/Complain.java
+++ manager/src/main/com/example/manage/bean/Complain/Complain.java
@@ -16,5 +16,9 @@ public class Complain {
     private int repair_id;
     private String owners_name; // 业主名字
     private String content; //投诉内容
+    private String dispatcher_name; //调度员名字
+    private String worker_name; //工人名字
+    private String dispatcher_explain; //调度员的解释
+    private String worker_explain; //工人的解释
     private int status;
 }
--- manager/src/test/java/example/manage/controller/ComplainTest.java
+++ manager/src/test/java/example/manage/controller/ComplainTest.java
@@ -25,40 +25,41 @@ public class ComplainTest {
 
     @Test
     void complain1() {
-        //用户提交投诉 (投诉表需新增字段repair_id以及status)
-        ownersController.launchComplain(repair_id, owners_name, content, status);
+        //用户提交投诉 dispatcher_explain和worker_explain置null, state为0打开状态, 假设投诉repair_id为1
+        ownersController.launchComplain("owners1", "content", "dispatcher1", "worker1", null, null, 0, 1);
 
         //获取用户投诉，进行比对
-        Complain complain = ownersController.getComplain(repair_id);
+        Complain complain = ownersController.getComplainByRepair_id(1);
 
-        assertEquals(complain.getRepair_id(), repair_id, "repair_id不一致");
-        assertEquals(complain.getOwners_name(), owners_name, "owners_name不一致");
-        assertEquals(complain.getContent(), content, "content不一致");
-        assertEquals(complain.getStatus(), status, "status不一致");
+        assertEquals(complain.getRepair_id(), 1, "repair_id不一致");
+        assertEquals(complain.getOwners_name(), "owners1", "owners_name不一致");
+        assertEquals(complain.getContent(), "content", "content不一致");
+        assertEquals(complain.getStatus(), 0, "status不一致");
 
         //根据维修id,找到相关调度员和维修工人
-        Workers workers = repairTableController.findWorkers(repair_id); //这边先只找一个
-        Dispatcher dispatcher = repairTableController.findDispatcher(repair_id);
+//        Workers workers = repairTableController.findWorkers(repair_id); //这边先只找一个
+//        Dispatcher dispatcher = repairTableController.findDispatcher(repair_id);
 
         //比对调度员和维修工人，是否正确
-        assertEquals(workers.getName(), "name", "工人name不一致");
-        assertEquals(workers.getType(), 0, "工人type不一致");
-        assertEquals(workers.getStatus(), 0, "工人status不一致");
-        assertEquals(dispatcher.getName(), "name", "调度员name不一致");
+//        assertEquals(workers.getName(), "worker_name", "工人name不一致");
+//        assertEquals(workers.getType(), 0, "工人type不一致");
+//        assertEquals(workers.getStatus(), 0, "工人status不一致");
+//        assertEquals(dispatcher.getName(), "dispatcher_name", "调度员name不一致");
+        assertEquals(complain.getDispatcher_name(), "dispatcher1", "调度员name不一致");
+        assertEquals(complain.getWorker_name(), "worker1", "工人name不一致");
 
-        // 提交情况说明
-        dispatcherController.launchExplain();
-        workersController.launchExplain();
+        // 提交情况说明 输入参数complain_id，以及情况说明content
+        dispatcherController.launchExplain(complain.getId(), "dispatcher_explain_content");
+        workersController.launchExplain(complain.getId(), "worker_explain_content");
 
         //获取情况说明，查看是否正确提交
+        assertEquals(complain.getDispatcher_explain(), "dispatcher_explain_content", "调度员情况说明不一致");
+        assertEquals(complain.getWorker_explain(), "worker_explain_content", "工人情况说明不一致");
 
-        情况说明写在哪张表里？
-
-        //更新投诉表，关闭该投诉
-        complainController.deleteComplain(complain_id);
+        //更新投诉表，关闭该投诉，状态置1
+        complainController.deleteComplain(complain.getId());
 
         //检查投诉是否正确关闭
-        Complain complain2 = ownersController.getComplain(repair_id);
-        assertEquals(complain.getStatus(), 0, "投诉未正确关闭");
+        assertEquals(complain.getStatus(), 1, "投诉未正确关闭");
     }
 }
